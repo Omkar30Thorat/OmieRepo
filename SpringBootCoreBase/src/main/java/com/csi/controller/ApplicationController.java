98bcbe4271fed58209107e1cf037d2fa146d2b29
Omkar30Thorat <omkarthorat2530@gmail.com>
2022-05-23 12:34:29 +0530

    DONE CHANGES

---
+++ SpringBootCoreBase/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,25 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ApplicationController {
+
+	@GetMapping
+	public String sayHello() {
+		return "WELCOME TO SAMARTH DEVELOPERS";
+	}
+
+	@GetMapping("/service")
+	public String sayService() {
+		return "DESIGN | DEVELOPMENT | INTERIOR";
+	}
+
+	@GetMapping("/address")
+	public String sayAddress() {
+		return "PARGAON | AMBEGAON | PUNE";
+	}
+}
